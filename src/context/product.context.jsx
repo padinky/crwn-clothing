9d72a3eff96f3890c9a062982701340792ced4f6
Eky Pradhana <tanyadhana@gmail.com>
2022-07-16 06:55:58 +0700

    shop ddaata

--- src/context/product.context.jsx
+++ src/context/product.context.jsx
@@ -1,6 +1,6 @@
 import { createContext,useState } from "react";
 
-import PRODUCTS from '../shop-data.json';
+import SHOP_DATA from '../shop-data.json';
 
 export const ProductContext = createContext({
     products: [],
@@ -8,7 +8,7 @@ export const ProductContext = createContext({
 });
 
 export const ProductProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
     const value = {products};
     return (
         <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
