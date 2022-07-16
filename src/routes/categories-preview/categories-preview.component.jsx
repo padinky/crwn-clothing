ab1ae152607d521f320a2c85bd30c3f4b4f216e8
Eky Pradhana <tanyadhana@gmail.com>
2022-07-16 08:20:02 +0700

    category preview (before nested routes)

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,21 +1,21 @@
 import { CategoriesContext } from '../../context/categories.context';
 import { useContext, Fragment } from 'react';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
-import './shop.styles.scss';
+import './categories-preview.styles.scss';
 
-const Shop = () => {
+const CategoriesPreview = () => {
     const { categoriesMap } = useContext(CategoriesContext);
     console.log("categories map from shop component == ", categoriesMap);
     return(
-        <div className='shop-container'>
+        <Fragment>
             {Object.keys(categoriesMap).map(title => {
                 const product = categoriesMap[title];
                 return (
                     <CategoryPreview key={title} title={title} products={product}  />
                 );
             })}
-        </div>
+        </Fragment>
     );
 };
 
-export default Shop;
+export default CategoriesPreview;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -11,7 +11,7 @@ import {
     onAuthStateChanged 
     } 
     from 'firebase/auth'
-import { getFirestore, doc, getDoc, setDoc, collection, writeBatch } from 'firebase/firestore'
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch, query, getDocs } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -47,6 +47,19 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
 
     await batch.commit();
     console.log("doneeeee");
+};
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot)=>{
+        const {title, items} = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    },{});
+
+    return categoryMap;
 };
 
 
